46de26939c4ad293add8d607bed29df7333fffdb
NateBergman <90053358+NateBergman@users.noreply.github.com>
2024-02-09 09:43:50 -0800

    finished 9

--- src/GoalSheet09.java
+++ src/GoalSheet09.java
@@ -8,12 +8,13 @@
 
 //Write a method called rarest that accepts a map whose keys are strings and whose values are integers as a parameter and returns the integer value that occurs the fewest times in the map.
 // If there is a tie, return the smaller integer value. If the map is empty, throw an exception
+import java.lang.reflect.Array;
 import java.util.*;
 public class GoalSheet09 {
     public static void main (String[] args) {
         LinkedList<Integer> list1 = new LinkedList<Integer>(Arrays.asList(1,6,5,3,8,5,3,7,3,8));
         ArrayList<Integer> list2 = new ArrayList<Integer>(Arrays.asList(1,6,5,3,8,5,3,7,3,8));
-        //partition(list1, 5);
+        partition(list1, 5);
         partition(list2, 5);
         System.out.println(list1);
         System.out.println(list2);
@@ -22,6 +23,33 @@ public class GoalSheet09 {
 
         Set<String> set1 = new HashSet<String>(Arrays.asList("123","1","12345","12"));
         System.out.println(maxLengh(set1));
+
+        Map<String,Integer> map = new HashMap<String,Integer>();
+        map.put("hello",1);
+        map.put("hi",3);
+        map.put("lol",2);
+        map.put("lala",1);
+        map.put("fefifofum",3);
+        System.out.print(rarest(map));
+    }
+    public static int rarest(Map<String,Integer> map) {
+        Map<Integer,Integer> counts = new HashMap<Integer, Integer>();
+        for (Integer i : map.values()) {
+            if (counts.containsKey(i)) {
+                counts.put(i,counts.get(i) + 1);
+            } else {
+                counts.put(i,1);
+            }
+        }
+        int smallestCount = 99999;
+        int smallestNum = -1;
+        for (Integer i : counts.keySet()) {
+            if (counts.get(i) < smallestCount) {
+                smallestNum = i;
+                smallestCount = counts.get(i);
+            }
+        }
+        return smallestNum;
     }
     public static int maxLengh (Set<String> set) {
         int max = 0;
@@ -62,18 +90,22 @@ public class GoalSheet09 {
     }
     public static void partition(LinkedList<Integer> list, int E) { //with LL, more efficient to remove/add
         Iterator<Integer> itr = list.iterator();
+        LinkedList<Integer> goodList = new LinkedList<Integer>();
         int i = 0;
-        while (itr.hasNext() && i < list.size()) {
+        while (itr.hasNext()) {
             int v = itr.next();
             if (v < E) {
-                itr.remove();
-                list.add(0,v);
+                goodList.add(0,v);
+                i++;
             }
-            if (v > E) {
-                itr.remove();
-                list.add(v);
+            else if (v > E) {
+                goodList.add(v);
+            } else {
+                goodList.add(i,v);
+                i++;
             }
-            i++;
         }
+        list.clear();
+        list.addAll(goodList);
     }
 }
---
+++ src/GoalSheet10.java
@@ -0,0 +1,13 @@
+//Write a recursive method called writeSquares that accepts an integer parameter n and prints the first n squares separated by commas, with the odd squares in descending order followed by the even squares in ascending order.
+// For example, writeSquares(8); prints the following output: 49, 25, 9, 1, 4, 16, 36, 64
+
+//Write a modified version of the selection sort algorithm that selects the largest element each time and moves it to the end of the array, rather than selecting the smallest element and moving it to
+// the beginning. Will this algorithm be faster than the standard selection sort? What will its complexity class (big-O) be?
+
+//Write a program that reads a series of input lines and sorts them into alphabetical order, ignoring the case of words. The program should use the merge sort algorithm so that it efficiently sorts even a large file.
+public class GoalSheet10 {
+    public static void main (String[] args) {
+
+    }
+    //public static void wrtie
+}
