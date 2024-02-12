ee24e2bc178bca7615f198ebe206a3b4803467f0
NateBergman <90053358+NateBergman@users.noreply.github.com>
2024-02-12 10:16:00 -0800

    Made some progress on 10, really just did everything I could without reading ch. 13 in the book

--- src/GoalSheet10.java
+++ src/GoalSheet10.java
@@ -4,10 +4,43 @@
 //Write a modified version of the selection sort algorithm that selects the largest element each time and moves it to the end of the array, rather than selecting the smallest element and moving it to
 // the beginning. Will this algorithm be faster than the standard selection sort? What will its complexity class (big-O) be?
 
+import java.util.Arrays;
+
 //Write a program that reads a series of input lines and sorts them into alphabetical order, ignoring the case of words. The program should use the merge sort algorithm so that it efficiently sorts even a large file.
 public class GoalSheet10 {
     public static void main (String[] args) {
-
+       /* for (int i = 1; i < 9; i++) {
+            writeSquares(i);
+            System.out.println();
+        }*/
+        int[] array1 = new int[] {1,8,7,5,3,8,-5,32,39,-2};
+        System.out.println(Arrays.toString(array1));
+        selectionSort(array1);
+        System.out.println(Arrays.toString(array1));
     }
-    //public static void wrtie
+    public static void selectionSort(int[] array) { //selection sort with n squared efficiency
+        for (int i = array.length - 1; i >= 0; i--) {
+            int maxValue = array[0];
+            int maxIndex = 0;
+            for (int j = 1; j <= i; j++) {
+                if (array[j] > maxValue) {
+                    maxIndex = j;
+                    maxValue = array[j];
+                }
+            }
+            array[maxIndex] = array[i];
+            array[i] = maxValue;
+        }
+    }
+    public static void writeSquares(int n) {
+        if (n > 0) {
+            if (n % 2 == 0) { //if even
+                writeSquares(n-1);
+                System.out.print(n*n + " ");
+            } else { //if odd
+                System.out.print(n*n + " ");
+                writeSquares(n-1);
+            }
+        }
+    }
 }
